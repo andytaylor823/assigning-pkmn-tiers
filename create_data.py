26b824e31b0584221f95db3d0956b5359628fa03
andytaylor823 <52831663+andytaylor823@users.noreply.github.com>
2019-09-03 14:14:51 -0400

    Update create_data.py

--- create_data.py
+++ create_data.py
@@ -17,18 +17,27 @@ del(brokenup[:7])			# delete unnecessary data before Abra
 # create an empty dictionary
 # this dictionary will return a dictionary of stats and such, and the key is the pkmn name
 dict_of_dicts = {}
-# all keys are [name, (stats), weight, height, types, abilities, formats, oob (no idea), cap, evos, alts, genfamily]
+
+# all keys given by the website are: [name, (stats), weight, height, types, abilities, formats, oob (no idea), cap, evos, alts, genfamily]
 # cap: is the pokemon part of the CAP project? all should be false
 # evos: lists all evolutions of a pokemon
-# alts: other possible "names" of the pokemon (mostly just if the Pokemon can Mega
+# alts: other possible "names" of the pokemon (mostly just if the Pokemon can Mega)
 # genfamily: what gens the Pokemon was in
+
+# the only keys we care about
 important_keys = ['hp', 'atk', 'def', 'spa', 'spd', 'spe', 'types', 'formats', 'evos']
+# list the types, then convert them to numbers
 types = ['Steel', 'Fairy', 'Dragon', 'Bug', 'Electric', 'Fire', 'Grass', 'Rock', 'Dark', 'Flying', 'Ground', 'Poison', 'Normal', 'Fighting', 'Ghost', 'Ice', 'Psychic', 'Water']
 dtypes = {types[i]:i for i in range(len(types))}
+# list all the tiers, then convert them to numbers
+# CAP and LC have negative values, since they don't really belong. 'Untiered' has a value of 0, and the tiers increase by 10
 tiers = ['CAP', 'LC', 'Untiered', 'PU', 'PUBL', 'NU', 'NUBL', 'RU', 'RUBL', 'UU', 'UUBL', 'OU', 'Uber', 'AG']
 dtiers = {tiers[i]:(10*(i-2)) for i in range(len(tiers))}
-for entry in brokenup:		# loops over each pokemon
 
+# loop over each Pokemon
+for entry in brokenup:	
+
+	# continue cleaning the data
 	# make each row have the form of a dictionary
 	a = entry.split(',')
 	b = [a[0]]
@@ -41,9 +50,11 @@ for entry in brokenup:		# loops over each pokemon
 	
 	# by now, each row should have a ':' in it
 	# all rows are characters, and the true strings have "" encasing them
-	d = {}
-	name = ''
-	for x in b:	# loops over each key:val pair in the pokemon
+	d = {}		# the dictionary particular to that pokemon
+	name = ''	# the pokemon's name
+	
+	# loop over each key:val pair in the pokemon
+	for x in b:
 		key, *val = x.split(':')
 
 		# clean up the key, perhaps skipping it
@@ -52,34 +63,41 @@ for entry in brokenup:		# loops over each pokemon
 		if key not in important_keys:	continue
 		val = val[0]
 		
+		# again, clean up the data
 		# if the val has multiple entries, create a proper array
 		if '[' in val:
 			val = val.strip('[').strip(']').split(',')
 			for i in range(len(val)):
 				val[i] = val[i].strip('"')
-				
+		
+		# convert the data to numbers
 		if '"' in val:		val.strip('"')
 		if type(val) != list:
 			if val.isdigit():	val = float(val)
 		
+		# add the key:val pair to the particular dictionary
 		d[key] = val
+	# add the finished dictionary to the total dictionary
 	dict_of_dicts[name] = d
 
 # columns of the csv are going to be:
-# name, (hp, atk, def, spa, spd, spe), (type1, type2), is_nfe, tier
 #   0     1   2    3    4    5    6      7       8       9      10
-fout = open('pokemonstats.csv', 'a')
+# name, (hp, atk, def, spa, spd, spe), (type1, type2), is_nfe, tier
 
 # write first line
+fout = open('pokemonstats.csv', 'a')
 fout.write('name,hp,atk,def,spa,spd,spe,type1,type2,is_nfe,tier\n')
-# write Abomasnow line
-#fout.write('Abomasnow,90.0,92.0,75.0,92.0,85.0,60.0,Grass,Ice,0,PU\n')
+
+# with how the data were scraped from the website, it makes it difficult to get the data for the first Pokemon, Abomasnow
+# write the data for this Pokemon by itself
 fout.write('Abomasnow,90.0,92.0,75.0,92.0,85.0,60.0,6,15,0,10\n')
 
-# write out each pokemon's info
+# Pokemon that aren't legal in Pokemon S/M competitive play
 bad_names = ['Floette-Eternal', 'Pikachu-Starter', 'Eevee-Starter', 'Meltan', 'Melmetal']
+
+# loop over each Pokemon
 for name in dict_of_dicts:
-	if name in bad_names:	continue
+	if name in bad_names:	continue	# skip the illegal pokemon
 	d = dict_of_dicts[name]
 	fout.write(name + ',')
 	
@@ -87,16 +105,11 @@ for name in dict_of_dicts:
 	for key in important_keys[:6]:
 		fout.write(str(d[key]) + ',')
 	
-	# write out types, write nothing if no second type
-	# now writing out just a number that corresponds to a certain type
+	# write out a number that corresponds to a certain type, write -1 if no second tpye
 	types = d['types']
 	fout.write(str(dtypes[types[0]]) + ',')
-	if len(types) > 2:
-		print('AHHHHH WHAT ', name)
-		exit(10)
 	if len(types) > 1:		fout.write(str(dtypes[types[1]]) + ',')
 	else:				fout.write('-1,')
-
 	
 	# check if the pokemon has an evo
 	if 'evos' not in d.keys():	has_evo = False
@@ -107,8 +120,8 @@ for name in dict_of_dicts:
 	if has_evo:			fout.write('1,')
 	else:				fout.write('0,')
 
-	# write out the tier
-	# now just writing out a number that corresponds to a tier -- increases
+	# write out a number that corresponds to a tier -- increases
+	# for some reason, Mega Sableye has an issue with its tier when scraped from the website -- write this in by hand
 	if d['formats'][0] == '':	T = 'Untiered'
 	elif name == 'Sableye-Mega':	T = 'UUBL'
 	else:				T = d['formats'][0]
@@ -118,11 +131,3 @@ for name in dict_of_dicts:
 
 fout.close()
 print('done')
-
-
-
-
-
-
-			
-			
