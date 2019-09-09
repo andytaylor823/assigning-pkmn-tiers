d427ca5fa996f533110c93557e2b81a6dbec6b6c
andytaylor823 <52831663+andytaylor823@users.noreply.github.com>
2019-09-09 17:09:51 -0400

    Update learn.py

--- learn.py
+++ learn.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import numpy as np
-from sklearn import svm		# bruce did this and used svm.SVC()
-from sklearn import naive_bayes
+from sklearn import svm
 import random
-from sklearn.metrics import mean_absolute_error as mae
 from sklearn import linear_model
 from sklearn import neighbors
+import matplotlib.pyplot as plt
 
-#random.seed(1638591)
+# create dictionaries to go from number <-> type and tier -> number
 random.seed(7089278395)
 types = ['Steel', 'Fairy', 'Dragon', 'Bug', 'Electric', 'Fire', 'Grass', 'Rock', 'Dark', 'Flying', 'Ground', 'Poison', 'Normal', 'Fighting', 'Ghost', 'Ice', 'Psychic', 'Water']
 dtypes = {types[i]:i for i in range(len(types))}
@@ -15,18 +14,21 @@ tiers = ['CAP', 'LC', 'Untiered', 'PU', 'PUBL', 'NU', 'NUBL', 'RU', 'RUBL', 'UU'
 dtiers = {tiers[i]:(10*(i-2)) for i in range(len(tiers))}
 num_to_tier = {(10*(i-2)):tiers[i] for i in range(len(tiers))}
 
-
+# read in the data
 df = pd.read_csv('pokemonstats.csv')
 #for t in ['Untiered', 'PU', 'NU', 'RU', 'UU', 'OU', 'Uber']:
 #	print('There are %i Pokemon in %s' %(sum(df['tier']==dtiers[t]), t))
 #print('------------------------')
+
+# clean out certain parts of the data
 df = df.loc[df['tier'] >= 0]			# excludes CAP and LC
 df = df.loc[df['is_nfe'] == 0]			# excludes NFE mons
 df = df.loc[df['tier'] > 0]			# excludes Untiered mons
 
+# the names of the pokemon
 names = np.array(df['name'])
 
-# after cleaning the data...
+# turn the data into a NumPy array and separate into training and testing
 data = np.array(df)
 train_idxs = np.array(random.sample(range(len(data)), int(0.9*len(data))))	# train on 90 % of the data
 test_idxs  = np.array([i for i in range(len(data)) if i not in train_idxs])
@@ -36,38 +38,40 @@ train_y = np.array(data[train_idxs, -1], dtype = float)
 test_X = np.array(data[test_idxs, 1:-1], dtype = float)
 test_y = np.array(data[test_idxs, -1], dtype = float)
 
+# uncomment these lines to only include certain parts of the data
 # only use BST
 #train_X, test_X = [arr[:,:6] for arr in [train_X, test_X]]
 
 # only use atk, spa, spe
 #train_X, test_X = [arr[:,[1, 3, 5]] for arr in [train_X, test_X]]
 
+# use 4 different fitting models to compare accuracy
+x = 2638952
+models = [svm.LinearSVC(random_state=x, max_iter=1e4), 
+	  linear_model.Lasso(random_state=x, alpha=0.1), 
+	  neighbors.KNeighborsClassifier(n_neighbors=5, weights='distance'),
+	  svm.SVR(kernel='linear')]
+titles = ['Linear Classification', 'Lasso Regression', 'K-Neighbors Classification', 'Linear Regression']
 
-#clf = svm.SVC()
-#clf = svm.LinearSVC(random_state=173950)
-clf = linear_model.Lasso(alpha = 0.1)		# this is pretty decent
-#clf = neighbors.KNeighborsClassifier(n_neighbors=10, weights='distance')	# this isn't that great
-clf.fit(train_X, train_y)
-predict_y = clf.predict(test_X)
+# train & test the models
+# plot the deviations from the correct values of each model
+fig = plt.figure()
+fig.add_subplot(111, frameon=False)
+plt.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
+plt.xlabel('Error', fontsize = 16)
+plt.ylabel('Density', fontsize = 16)
+for i in range(len(titles)):
+	ax = fig.add_subplot(2, 2, i+1)
+	clf = models[i]
+	clf.fit(train_X, train_y)
+	predict_y = clf.predict(test_X)
+	diff = test_y - predict_y
+	ax.hist(diff, histtype = 'step', bins = 15, density=True)#, label = titles[i])
+	ax.set_title(titles[i])
 
+	closeness = 20
+	nright = sum(abs(predict_y-test_y)<=closeness)
+	print('%s predicted with %.1f percent accuracy' %(titles[i], nright/len(predict_y)*100))
 
-closeness = 20
-nright = sum(abs(test_y - predict_y) < closeness)
-print('This was %.1f percent right' %(nright/len(test_y)*100))
-print('Trained on', len(train_y), 'data points')
-print('Tested on ', len(test_y), ' data points')
-print('MAE = %.1f' %mae(test_y, predict_y))
-print('--------------------------------------------')
-
-for i in range(len(test_y)):
-
-	py = predict_y[i]
-	ty = test_y[i]
-	# check if they're farther away +/- some amount of the right value
-	if abs(py - ty) > closeness:
-		name = names[test_idxs[i]]
-
-		line = name
-		for k in range(3, len(name)//8, -1):	line += '\t'
-		line += '\t%.1f\t%.1f'
-		print(line %(ty, py))
+plt.tight_layout()
+plt.show()
